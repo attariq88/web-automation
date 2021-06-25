becaec5c9b30874b9016e90f48b3397a5bd58a3e
attariq88 <tarik88sep@gmail.com>
2021-06-25 17:53:22 -0400

    Create account test

--- amazon/src/test/java/com/amazon/tests/CreateAccountTest.java
+++ amazon/src/test/java/com/amazon/tests/CreateAccountTest.java
@@ -1,5 +1,6 @@
 package com.amazon.tests;
 
+import com.amazon.base.ConnectDB;
 import com.amazon.base.TestBase;
 import com.amazon.base.report.ExtentTestManager;
 import com.amazon.data.DataProviderForTests;
@@ -10,6 +11,9 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class CreateAccountTest extends TestBase {
     private static final Logger LOGGER = Logger.getLogger(CreateAccountTest.class);
@@ -26,8 +30,8 @@ public class CreateAccountTest extends TestBase {
 
     }
 
-    @Test(dataProviderClass = DataProviderForTests.class,dataProvider = "getDataForCreateAccountTest")
-    public void validateUserCanCreateAccount(String userName, String email,String password){
+    @Test(enabled = false,dataProviderClass = DataProviderForTests.class,dataProvider = "getDataForCreateAccountTest")
+    public void validateUserCanCreateAccountFromDataProvider(String userName, String email,String password){
 
         validateUrlWithExpected("https://www.amazon.com/");
         amazonLogoIsDisplayed();
@@ -42,15 +46,19 @@ public class CreateAccountTest extends TestBase {
 
         createAccountPage.typeInNameField(userName);
         ExtentTestManager.log("user name typed in successfully",LOGGER);
+        sleepFor(1);
 
         createAccountPage.typeInMobileNumberOrEmailField(email);
         ExtentTestManager.log("email address typed in successfully",LOGGER);
+        sleepFor(1);
 
         createAccountPage.typeInPasswordFiled(password);
         ExtentTestManager.log("password typed in successfully",LOGGER);
+        sleepFor(1);
 
         createAccountPage.typeInReEnterField(password);
         ExtentTestManager.log("password re-entered successfully",LOGGER);
+        sleepFor(1);
 
         createAccountPage.validateTheFieldLabelsAreDisplayed();
         ExtentTestManager.log("Field's label are displayed",LOGGER);
@@ -61,6 +69,49 @@ public class CreateAccountTest extends TestBase {
         sleepFor(10);
 
 //        createAccountPage.clickOnSolePuzzleButton();
+
+    }
+
+    @Test(enabled = true)
+    public void validateUserBeingAbleToCreateAccountDatabase() throws SQLException {
+
+        String query = "select * from information_for_account_create";
+
+        ArrayList<String> username = ConnectDB.connectToDbAndGetTheData(query,"username");
+        ArrayList<String> email = ConnectDB.connectToDbAndGetTheData(query,"email");
+        ArrayList<String> password = ConnectDB.connectToDbAndGetTheData(query,"password");
+
+        validateUrlWithExpected("https://www.amazon.com/");
+        homePage.amazonLogoIsDisplayed();
+
+        homePage.clickOnSignButton();
+        ExtentTestManager.log("Sign in button clicked.",LOGGER);
+        sleepFor(2);
+
+        signInPage.clickOnCreateAccountButton();
+        ExtentTestManager.log("Create your Amazon account clicked",LOGGER);
+        sleepFor(2);
+
+        createAccountPage.typeInNameField(username.get(0));
+        ExtentTestManager.log(username.get(0) + " typed on the name field.",LOGGER);
+        sleepFor(1);
+
+        createAccountPage.typeInMobileNumberOrEmailField(email.get(0));
+        ExtentTestManager.log(email.get(0) + " typed on the email field.",LOGGER);
+        sleepFor(1);
+
+        createAccountPage.typeInPasswordFiled(password.get(0));
+        ExtentTestManager.log(password.get(0) + " typed on the password field.",LOGGER);
+        sleepFor(1);
+
+        createAccountPage.typeInReEnterField(password.get(0));
+        ExtentTestManager.log(password.get(0) + " typed on the re-enter password field.",LOGGER);
+        sleepFor(1);
+
+        createAccountPage.clickOnVerifyEmailButton();
+        ExtentTestManager.log("verify email button clicked successfully",LOGGER);
+        sleepFor(10);
+
 
     }
 
