e6eb281b9000ee34cb0b90c70a4bebc4cb61a31f
attariq88 <tarik88sep@gmail.com>
2021-06-26 19:42:04 -0400

    Initial commit

---
+++ facebook/src/main/java/com/facebook/pages/AccountInfoPage.java
@@ -0,0 +1,10 @@
+package com.facebook.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AccountInfoPage {
+
+    @FindBy(xpath = "Continue")
+    private WebElement continueButton;
+}
